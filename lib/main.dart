5dc46cbbfde89751a7f6139da31ef522d49493e6
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-01-07 21:04:13 +0700

    formatting dates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -108,7 +109,7 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        transaction.date.toString(),
+                        DateFormat('yyyy/MM/dd').format(transaction.date),
                         style: TextStyle(
                           color: Colors.grey,
                         ),
