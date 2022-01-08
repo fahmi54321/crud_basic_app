4165b379a092120f9da356037d4860007597012c
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-01-08 12:08:36 +0700

    working with listviews

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -13,57 +13,55 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((transaction) {
-            return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      child: Text(
-                        '\$${transaction.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(
-                        10,
+      child: ListView(
+        children: transactions.map((transaction) {
+          return Card(
+              child: Row(
+                children: [
+                  Container(
+                    child: Text(
+                      '\$${transaction.amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          transaction.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(
+                      10,
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transaction.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          DateFormat('yyyy/MM/dd').format(transaction.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
+                      ),
+                      Text(
+                        DateFormat('yyyy/MM/dd').format(transaction.date),
+                        style: TextStyle(
+                          color: Colors.grey,
                         ),
-                      ],
-                    )
-                  ],
-                ));
-          }).toList(),
-        ),
+                      ),
+                    ],
+                  )
+                ],
+              ));
+        }).toList(),
       ),
     );
   }
