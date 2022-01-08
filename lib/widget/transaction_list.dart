49a667fc8676091b68429d08ce8b53f38637dc0e
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-01-08 23:09:30 +0700

    adding images to the app

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -4,63 +4,79 @@ import '../models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class TransactionList extends StatelessWidget {
-
-
   final List<Transaction> transactions;
+
   TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx,index){
-          return Card(
-              child: Row(
-                children: [
-                  Container(
-                    child: Text(
-                      '\$${transactions[index].amount.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(
-                      10,
-                    ),
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No transactions added yet',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset(
+                    'assets/images/icon_nopic.png',
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.headline1,
-                      ),
-                      Text(
-                        DateFormat('yyyy/MM/dd').format(transactions[index].date),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                      child: Text(
+                        '\$${transactions[index].amount.toStringAsFixed(2)}',
                         style: TextStyle(
-                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor,
                         ),
                       ),
-                    ],
-                  )
-                ],
-              ));
-        },
-      ),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).primaryColor,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(
+                        10,
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          transactions[index].title,
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
+                        Text(
+                          DateFormat('yyyy/MM/dd')
+                              .format(transactions[index].date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ));
+              },
+            ),
     );
   }
 }
